056680bec9b6197e5db511ee50578949a9d2cbab
Jeniffer Azevedo <jeniqf@gmail.com>
2020-08-14 19:15:15 +0100

    Orders code

--- src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -3,6 +3,7 @@ import { getRepository, Repository, In } from 'typeorm';
 import IProductsRepository from '@modules/products/repositories/IProductsRepository';
 import ICreateProductDTO from '@modules/products/dtos/ICreateProductDTO';
 import IUpdateProductsQuantityDTO from '@modules/products/dtos/IUpdateProductsQuantityDTO';
+import AppError from '@shared/errors/AppError';
 import Product from '../entities/Product';
 
 interface IFindProducts {
@@ -41,12 +42,39 @@ class ProductsRepository implements IProductsRepository {
     return findProductByName;
   }
 
-  public async findAllById(products: IFindProducts[]): Promise<Product[]> {}
+  public async findAllById(products: IFindProducts[]): Promise<Product[]> {
+    const idProductList = products.map(product => product.id);
+    const orderList = await this.ormRepository.find({ id: In(idProductList) });
+
+    if (idProductList.length !== orderList.length) {
+      throw new AppError('Some products are not registered!');
+    }
+    return orderList;
+  }
 
   public async updateQuantity(
     products: IUpdateProductsQuantityDTO[],
   ): Promise<Product[]> {
-    // TODO
+    const productsData = await this.findAllById(products);
+    const updateProducts = productsData.map(productData => {
+      const productFind = products.find(
+        product => product.id === productData.id,
+      );
+      if (!productFind) {
+        throw new AppError('Product not found');
+      }
+      if (productData.quantity < productFind.quantity) {
+        throw new AppError('There is no such quantity in stock.');
+      }
+      const newProduct = productData;
+      newProduct.quantity -= productFind.quantity;
+
+      return newProduct;
+    });
+
+    await this.ormRepository.save(updateProducts);
+
+    return updateProducts;
   }
 }
 
